ad1e74d09bb21eb74194a1d931e26619331d5850
Deepika <guliani.deepika@gmail.com>
2015-01-05 12:21:54 +0530

    edited json file

---
+++ fs2tree/input.js
@@ -0,0 +1,71 @@
+var flatlist = [
+    {
+        "element": {
+            "type": "folder",
+            "label": "Name1",
+            "parent": null,
+            "id": "1"
+        }
+    },
+
+    {
+        "element": {
+            "type": "folder",
+            "label": "Name2",
+            "parent": null,
+            "id": "2",
+            "parent": {
+                "type": "folder",
+                "label": "Name1",
+                "parent": null,
+                "id": "1"
+            }
+        }
+    },
+
+    {
+        "element": {
+            "type": "folder",
+            "label": "Name3",
+            "parent": null,
+            "id": "3",
+            "parent": {
+                "type": "folder",
+                "label": "Name1",
+                "parent": null,
+                "id": "1"
+            }
+        }
+    },
+
+    {
+        "element": {
+            "type": "folder",
+            "label": "Name4",
+            "parent": null,
+            "id": "4",
+            "parent": {
+                "type": "folder",
+                "label": "Name2",
+                "parent": null,
+                "id": "2"
+            }
+        }
+    },
+
+    {
+        "element": {
+            "type": "folder",
+            "label": "Name5",
+            "parent": null,
+            "id": "5",
+            "parent": {
+                "type": "folder",
+                "label": "Name3",
+                "parent": null,
+                "id": "3"
+            }
+        }
+    },
+
+]
